caebf7496f547d8962dde90e1b0f503650aa3912
Bua2007 <fopbukachalisa@gmail.com>
2025-03-27 09:00:39 +0200

    add redux persist for state of CompleteQuiz

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,37 @@ import { configureStore } from "@reduxjs/toolkit";
 import { quizReducer } from "./quiz/slice.js";
 import { filtersReducer } from './filters/slice.js';
 import {completeQuizReducer} from './completeQuiz/slice.js'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const completeQuizPersistConfig = {
+  key: 'completeQuiz',
+  storage,
+  whitelist: ['answers'],
+};
 
 export const store = configureStore({
   reducer: {
     quiz: quizReducer,
     filters: filtersReducer,
-    completeQuiz:completeQuizReducer,
+    completeQuiz:persistReducer(completeQuizPersistConfig,completeQuizReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
